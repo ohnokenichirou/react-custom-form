9726a759fe550894506d9be00578d2221ad2bf66
ohnokenichirou <99148127+ohno-kenichirou@users.noreply.github.com>
2024-09-24 18:20:40 +0900

    Reactでログインフォームを自作してみよう【HTMLからCSSまで解説】 - プログラミングチュートリアル

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const initialValues = { username: "", mailAddress: "", passsword: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmint, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //ログイン情報を送信する。
+    //バリデーションチェックをする。
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  const validate = (values) => {
+    const errors = {};
+    const regex =
+      /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/;
+    if (!values.username) {
+      errors.username = "ユーザー名を入力して下さい";
+    }
+    if (!values.mailAddress) {
+      errors.mailAddress = "メールアドレスを入力して下さい";
+    } else if (!regex.test(values.mailAddress)) {
+      errors.mailAddress = "正しいメールアドレスを入力して下さい";
+    }
+    if (!values.passsword) {
+      errors.passsword = "パスワードを入力して下さい";
+    } else if (values.passsword.length < 4) {
+      errors.passsword = "4文字以上15文字以下のパスワードを入力して下さい";
+    } else if (values.passsword.length > 15) {
+      errors.passsword = "4文字以上15文字以下のパスワードを入力して下さい";
+    }
+    return errors;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="formContainer">
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <h1>ログインフォーム</h1>
+        <hr />
+        <div className="uiForm">
+          <div className="formField">
+            <label>ユーザー名</label>
+            <input
+              type="text"
+              placeholder="ユーザー名"
+              name="username"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          <p className="errorMsg">{formErrors.username}</p>
+          <div className="formField">
+            <label>メールアドレス</label>
+            <input
+              type="text"
+              placeholder="メールアドレス"
+              name="mailAddress"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          <p className="errorMsg">{formErrors.mailAddress}</p>
+          <div className="formField">
+            <label>パスワード</label>
+            <input
+              type="text"
+              placeholder="パスワード"
+              name="passsword"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          <p className="errorMsg">{formErrors.passsword}</p>
+          <button className="submitButton">ログイン</button>
+          {Object.keys(formErrors).length === 0 && isSubmint && (
+            <div className="msgOk">ログインに成功しました</div>
+          )}
+        </div>
+      </form>
     </div>
   );
 }
